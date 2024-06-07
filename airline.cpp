30bf737e35618b22d6c16be9e14edaabee198a69
sanjaraiy <deepakrai78788@gmail.com>
2024-06-07 20:21:19 +0530

    setup an airline Resversation Ticket Project

---
+++ airline.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+using namespace std;
+
+void mainMenu(){
+   int lchoice;
+   int schoice;
+   int back;
+
+   cout<<"\t             VISTARA Airlines \n"<<endl;
+   cout<<"\t_____________ Main Menu _________________"<<endl;
+}
+
+
+
+class Management {
+    public:
+    
+    //Constructor
+    Management(){
+       mainMenu();
+    }
+
+
+};
+
+int main(){
+    
+}
